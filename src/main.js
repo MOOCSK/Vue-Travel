f4df5c40cd9b5df4047920fc285b46cdfe447397
moocsk <moocsk@163.com>
2018-08-07 21:31:08 +0800

    solve 300ms timeout

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import Vue from 'vue';
 import router from './router';
 import App from './App';
 
+import fastClick from 'fastclick';
+// 解决移动端点击300ms延迟
+fastClick.attach(document.body);
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
