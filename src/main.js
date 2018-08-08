c4e68cc241fd38486fa7216b575e48b4a27a58bc
moocsk <moocsk@163.com>
2018-08-08 23:30:44 +0800

    add swiper

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/dist/css/swiper.css';
 import router from './router';
 import App from './App';
 
@@ -11,6 +13,7 @@ import fastClick from 'fastclick';
 // 解决移动端点击300ms延迟
 fastClick.attach(document.body);
 
+Vue.use(VueAwesomeSwiper);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl">
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+import Img01 from '@/assets/imgs/img_01.jpg';
+import Img02 from '@/assets/imgs/img_02.jpg';
+
+export default {
+    data() {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [
+                {
+                    id: '0001',
+                    imgUrl: Img01
+                },
+                {
+                    id: '0002',
+                    imgUrl: Img02
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style>
+.wrapper .swiper-pagination-bullet-active {
+    background-color: #fff;
+}
+</style>
+<style lang="less" scoped>
+.wrapper {
+    width: 100%;
+    height: 0;
+    padding-bottom: 25.6%;
+    overflow: hidden;
+    background-color: #ccc;
+}
+.swiper-img {
+    width: 100%;
+}
+</style>
