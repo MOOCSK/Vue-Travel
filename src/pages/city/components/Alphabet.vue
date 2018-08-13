d96cb594c404d5ec12a2ec9f48bd6d854beb9ca8
moocsk <moocsk@163.com>
2018-08-13 22:31:51 +0800

    city alphabet

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,30 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">C</li>
+        <li class="item">D</li>
+    </ul>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style lang="less" scoped>
+.list {
+    position: absolute;
+    top: 1.58rem;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: .4rem;
+}
+.item {
+    line-height: .4rem;
+    text-align: center;
+    color: @bgColor;
+}
+</style>
