8cc38b7162b5ca3bcb6996e0a6b6b3f1371212d2
moocsk <moocsk@163.com>
2018-08-13 22:19:35 +0800

    city list

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="list">
+        <div class="area">
+            <div class="title border-topbottom">当前城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">苏州</div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">热门城市</div>
+            <div class="button-list">
+                <div class="button-wrapper">
+                    <div class="button">北京</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">上海</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">广州</div>
+                </div>
+                <div class="button-wrapper">
+                    <div class="button">深圳</div>
+                </div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+            </div>
+        </div>
+        <div class="area">
+            <div class="title border-topbottom">A</div>
+            <div class="item-list">
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+                <div class="item border-bottom">苏州</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style lang="less" scoped>
+.border-topbottom {
+  &:before {
+    border-color: #ccc;
+  }
+  &:after {
+    border-color: #ccc;
+  }
+}
+.border-bottom {
+  &:before {
+    border-color: #ccc;
+  }
+}
+.list {
+    position: absolute;
+    top: 1.58rem;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    overflow: hidden;
+}
+.title {
+  line-height: 0.44rem;
+  padding-left: 0.2rem;
+  font-size: 0.26rem;
+  color: #666;
+  background-color: #eee;
+}
+.button-list {
+  padding: 0.1rem 0.6rem 0.1rem 0.1rem;
+  overflow: hidden;
+}
+.button-wrapper {
+  float: left;
+  width: 33.33%;
+}
+.button {
+  padding: 0.1rem 0;
+  margin: 0.1rem;
+  border-radius: 0.06rem;
+  border: 0.02rem solid #ccc;
+  text-align: center;
+}
+.item-list {
+
+}
+.item {
+    line-height: .76rem;
+    padding-left: .2rem;
+}
+</style>
