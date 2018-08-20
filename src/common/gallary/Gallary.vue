c101844faf0160608aef5564d62229876e9db437
moocsk <moocsk@163.com>
2018-08-20 21:16:26 +0800

    gallary

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container" @click="handleGallaryClick">
+        <div class="wrapper">
+            <swiper :options="swiperOption">
+                <swiper-slide v-for="(item, index) of imgs"
+                              :key="index">
+                    <img class="gallary-img" :src="item" />
+                </swiper-slide>
+                <div class="swiper-pagination" slot="pagination"></div>
+            </swiper>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        imgs: {
+            type: Array,
+            default () {
+                return [];
+            }
+        }
+    },
+    data() {
+        return {
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                paginationType: 'fraction',
+                observeParents: true,
+                observer: true
+            }
+        };
+    },
+    methods: {
+        handleGallaryClick() {
+            this.$emit('close');
+        }
+    }
+};
+</script>
+<style>
+    .container .swiper-container {
+        overflow: inherit;
+    }
+</style>
+<style lang="less" scoped>
+    .container {
+        position: fixed;
+        z-index: 99;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        background-color: #000;
+    }
+    .wrapper {
+        width: 100%;
+        height: 0;
+        padding-bottom: 99%;
+        background-color: #fff;
+    }
+    .gallary-img {
+        width: 100%;
+    }
+    .swiper-pagination {
+        color: #fff;
+        bottom: -1rem;
+    }
+</style>
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,20 +1,44 @@
 <template>
-    <div class="banner">
-        <img class="banner-img" src="https://note.youdao.com/yws/api/personal/file/C17CB0217C2D45D9BC40E2889248E9DD?method=download&shareKey=01fc6a10096e3d7f4517f7865ae202fa" />
-        <div class="banner-info">
-            <div class="banner-title">
-                大连圣亚海洋世界（AAAA景区）
-            </div>
-            <div class="banner-number">
-                <span class="iconfont icon-img"></span>
-                39
+    <div>
+        <div class="banner" @click="handleBannerClick">
+            <img class="banner-img" src="https://note.youdao.com/yws/api/personal/file/C17CB0217C2D45D9BC40E2889248E9DD?method=download&shareKey=01fc6a10096e3d7f4517f7865ae202fa" />
+            <div class="banner-info">
+                <div class="banner-title">
+                    大连圣亚海洋世界（AAAA景区）
+                </div>
+                <div class="banner-number">
+                    <span class="iconfont icon-img"></span>
+                    39
+                </div>
             </div>
         </div>
+        <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
     </div>
 </template>
 <script>
-export default {
+import CommonGallary from 'common/gallary/Gallary';
 
+export default {
+    components: {
+        CommonGallary
+    },
+    data() {
+        return {
+            imgs: [
+                'https://note.youdao.com/yws/api/personal/file/525537D1952E45BC9CC192A1796ABD57?method=download&shareKey=83c615f688ee28ca560dd09fbed5edc1',
+                'https://note.youdao.com/yws/api/personal/file/CE75A5597BD44D3498031328EF890012?method=download&shareKey=2e93963703ce837e187b1b36f1c7a671'
+            ],
+            showGallary: false
+        };
+    },
+    methods: {
+        handleBannerClick() {
+            this.showGallary = true;
+        },
+        handleGallaryClose() {
+            this.showGallary = false;
+        }
+    }
 };
 </script>
 <style lang="less" scoped>
