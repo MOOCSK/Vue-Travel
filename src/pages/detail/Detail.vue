5b4b840dbf7b6d44cff125735b622a2ddeb2708b
moocsk <moocsk@163.com>
2018-08-20 23:04:05 +0800

    detail header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,17 +1,22 @@
 <template>
     <div>
         <detail-banner></detail-banner>
+        <detail-header></detail-header>
+        <div class="content"></div>
     </div>
 </template>
 <script>
 import DetailBanner from './components/Banner';
-
+import DetailHeader from './components/Header';
 export default {
     components: {
-        DetailBanner
+        DetailBanner,
+        DetailHeader
     }
 };
 </script>
 <style lang="less" scoped>
-
+    .content {
+        height: 15rem;
+    }
 </style>
